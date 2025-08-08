b2d190da13a609707d102983ed5660005aee1851
Rahul <rahul@Rahuls-MacBook-Air-3.local>
2025-08-08 16:58:45 +0530

    We started adding full file path

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const path = require('path');
-
+fileStack = [];
 module.exports = function ({ types: t }) {
   return {
     visitor: {
+      Program: {
+        enter(_, state) {
+          const filename = state.file.opts.filename;
+          if (!filename) return;
+
+          const relativePath = path.relative(process.cwd(), filename);
+
+          fileStack.push(relativePath);
+        }
+      },
       JSXOpeningElement(pathNode, state) {
-        const filename = state.file.opts.filename;
 
-        if (!filename) return;
-
-        const relativePath = path.relative(process.cwd(), filename);
-
-        const alreadyHas = pathNode.node.attributes.some(attr =>
-          t.isJSXAttribute(attr) && attr.name.name === 'file-path'
+        const formattedPath = fileStack.join(' > ');
+        pathNode.node.attributes.push(
+          t.jsxAttribute(
+            t.jsxIdentifier('file-path'),
+            t.stringLiteral(`${formattedPath}`)
+          )
         );
 
-        if (!alreadyHas) {
-          pathNode.node.attributes.push(
-            t.jsxAttribute(
-              t.jsxIdentifier('file-path'),
-              t.stringLiteral(relativePath)
-            )
-          );
-        }
+      },
+      JSXClosingElement(pathNode, state) {
       }
     }
   };
